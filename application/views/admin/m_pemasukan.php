59b5986e39eb1b980ba4025b15c38b1c38b378ca
aa43000 <ahmadzaenimubarok121@gmail.com>
2022-05-29 10:16:08 +0700

    menambahkan master pemasukan

---
+++ application/views/admin/m_pemasukan.php
@@ -0,0 +1,177 @@
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Master Pemasukan</h1>
+
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex">
+              <h6 class="m-0 font-weight-bold text-primary">Tabel Master Pemasukan</h6>
+              <a href="#" class="btn btn-primary btn-icon-split btn-sm ml-3" data-toggle="modal" data-target="#formModal">
+                <span class="icon text-white-50"><i class="fas fa-plus"></i></span>
+                <span class="text">Tambah data</span>
+              </a>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="table1" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th width="10%">No</th>
+                      <th width="80%">Nama</th>
+                      <th width="10%">Action</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                  <tr>
+                      <th width="10%">No</th>
+                      <th width="80%">Nama</th>
+                      <th width="10%">Action</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          
+          
+          </div>
+    <!-- /.container-fluid -->
+    
+    <!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form tambah data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="formAll">
+            <input type="hidden" name="id" id="id" value="">
+
+          <div class="form-group">
+            <label for="header">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+          </div>
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="save()">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        load_data();
+    });
+    
+    function load_data() {
+        $("#table1").DataTable({
+            destroy: true,
+            "processing" : true,
+            "serverSide" : true,
+            ajax :{
+                url: "<?= base_url('M_pemasukan/load_data') ?>"
+            },
+            "columns" :[
+                {"name" : "id"},
+                {"name" : "nama"},
+                {"name" : "action", "orderable": false, "searchlable": false, "className": "text-center"}
+            ],
+            "order" : [
+                [0, 'asc']
+            ],
+            "iDisplayLength" : 10
+        });
+    }
+
+    function save() {
+      var formData = new FormData($("#formAll")[0]);
+      $.ajax({
+        url: "<?= base_url('M_pemasukan/tambah') ?>",
+        data: formData,
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        type: 'post'
+      }).done(function(data) {
+        if(data.status == 200) {
+          swal({
+            title: "Berhasil!",
+            text: data.message,
+            icon: "success",
+            button: "oke",
+          }).then((status)=>{
+            $("#formModal").modal('hide');
+            reset_form();
+            load_data();
+          });
+        } else if(data.status == 202) {
+          swal("Warning", data.message, "warning");
+        }
+      }) 
+    }
+
+    function reset_form() {
+    $("#id").val(0);
+      $("#nama").val("");
+    }
+
+    function edit_data(id) {
+        $.ajax({
+            url: "<?= base_url('M_pemasukan/edit_data/') ?>"+id,
+            dataType: 'json',
+            type: 'post'
+        }).done(function(data) {
+            $("#formModal").modal('show');
+            $("#nama").val(data.value.nama);
+            $("#id").val(data.value.id);
+        });
+    }
+
+    function delete_data(id){
+      swal({
+        title: "Anda yakin?",
+        text: "Data yang telah dihapus tidak dapat dipulihkan!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          $.ajax({
+            url: "<?= base_url('M_pemasukan/delete_data/') ?>"+id,
+            dataType: 'json',
+            type: 'post'
+          }).done(function(data) {
+            if(data.status == 200) {
+              swal({
+                title: data.title,
+                text: data.message,
+                icon: 'success',
+                button: 'oke'
+              }).then((a)=>{
+                load_data();
+              });
+            } else {
+              swal('Error', data.message, 'error');
+            }
+          });
+        } else {
+          swal("Data Tidak jadi dihapus!");
+        }
+      });
+    }
+</script>
