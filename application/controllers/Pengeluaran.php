50f610fa0fbbf32f34701f574bcea71ad8b63966
aa43000 <ahmadzaenimubarok121@gmail.com>
2022-05-31 19:24:18 +0700

    menambahkan module pemasukan dan pengeluaran

---
+++ application/controllers/Pengeluaran.php
@@ -0,0 +1,222 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pengeluaran extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata("id_user")) {
+            if($this->session->userdata("user_type") == 1) {
+
+            } else {
+                redirect(base_url('welcome/admin'));
+            }
+        } else {
+            redirect(base_url('login'));
+        }
+	}
+	public function index()
+	{
+        $data['judul'] = 'Pengeluaran';
+        $data['konten'] = $this->load->view('admin/pengeluaran', $data, true);
+		$this->load->view('template/index', $data);
+	}
+    
+    public function load_data() {
+	$select = "a.*, b.nama";
+	//LIMIT
+	$limit = array(
+		'start'  => $this->input->get('start'),
+		'finish' => $this->input->get('length')
+	);
+	//WHERE LIKE
+	$where_like['data'][] = array(
+		'column' => 'a.kode, b.nama',
+		'param'	 => $this->input->get('search[value]')
+	);
+	//ORDER
+	$index_order = $this->input->get('order[0][column]');
+	$order['data'][] = array(
+		'column' => $this->input->get('columns['.$index_order.'][name]'),
+		'type'	 => $this->input->get('order[0][dir]')
+	);
+
+	$join['data'][] = array(
+		'table' => 'm_pengeluaran b',
+		'join'	=> 'b.id=a.idm_pengeluaran',
+		'type'	=> 'left'
+	);
+
+	$where['data'][]=array(
+		'column'	=>'a.status_delete',
+		'param'		=>0
+	);
+
+	$query_total = $this->Modelku->select($select,'pengeluaran a',NULL,NULL,NULL,$join,$where);
+	$query_filter = $this->Modelku->select($select,'pengeluaran a',NULL,$where_like,$order,$join,$where);
+	$query = $this->Modelku->select($select,'pengeluaran a',$limit,$where_like,$order,$join,$where);
+	$response['data'] = array();
+	if ($query<>false) {
+		$no = $limit['start']+1;
+		foreach ($query->result() as $val) {
+			if ($val->id>0) {
+                $edit = '<button class="btn btn-circle btn-sm btn-success" type="button" onclick="edit_data('.$val->id.')"><i class="fas fa-edit"></i></button>';
+                $delete = '<button class="btn btn-circle btn-sm btn-danger" type="button" onclick="delete_data('.$val->id.')"><i class="fas fa-trash"></i></button>';
+				
+				$response['data'][] = array(
+                    $no,
+                    $val->kode,
+                    $val->nama,
+                    $val->tanggal,
+                    "Rp. ".number_format($val->jumlah, 0, ',', '.'),
+                    $val->operator,
+                    $edit." ".$delete
+				);
+				$no++;	
+			}
+		}
+	}
+
+	$response['recordsTotal'] = 0;
+	if ($query_total<>false) {
+		$response['recordsTotal'] = $query_total->num_rows();
+	}
+	$response['recordsFiltered'] = 0;
+	if ($query_filter<>false) {
+		$response['recordsFiltered'] = $query_filter->num_rows();
+	}
+
+	echo json_encode($response);
+	}
+
+    public function tambah() {
+        if($this->input->post('id') > 0) {
+            $data = $this->general_input_post();
+            // $data['kode'] = $this->_get_code();
+            $where = [
+                'id' => $this->input->post('id')
+            ];
+            $update = $this->Modelku->update('pengeluaran', $data, $where);
+            if($update<>false) {
+                $response['status'] = '200';
+                $response['message'] = 'Data berhasil di update';
+            } else {
+                $response['status'] = '202';
+                $response['message'] = 'gagal update data';
+            }
+        } else {
+            $data = $this->general_input_post();
+            $data['kode'] = $this->_get_code();
+            $insert = $this->db->insert('pengeluaran', $data);
+            if($insert<>false) {
+                $response['status'] = '200';
+                $response['message'] = 'Berhasil menambahkan data';
+            } else {
+                $response['status'] = '202';
+                $response['message'] = 'gagal tambah data';
+            }
+        }
+        
+
+        echo json_encode($response);
+    }
+
+    function general_input_post() {
+        $data = [
+            'idm_pengeluaran' => $this->input->post('idm_pengeluaran'),
+            'tanggal' => $this->input->post('tanggal'),
+            'jumlah' => $this->_convert($this->input->post('jumlah')),
+            'operator' => $this->session->userdata("username"),
+        ];
+
+        return $data;
+    }
+    public function _convert($angka) {
+        $angka_str = preg_replace("/[^0-9]/", "", $angka);
+        return (int) $angka_str;
+    }
+    public function _get_code()
+    {
+        $bln = date('m');
+        $thn = date('y');
+        $query =  $this->db->query("SELECT CAST(MID(kode, 10, 4) AS UNSIGNED) AS ids FROM pengeluaran WHERE kode LIKE CONCAT('PNG/', '$thn', '$bln', '/', '%') ORDER BY kode DESC LIMIT 1")->row();
+        if ($query) {
+            if ($query->ids<9) {
+                $nomor = $query->ids+1;
+                $seq = '000'.$nomor;
+            } elseif ($query->ids<99) {
+                $nomor = $query->ids+1;
+                $seq = '00'.$nomor;
+            } elseif ($query->ids<999) {
+                $nomor = $query->ids+1;
+                $seq = '0'.$nomor;
+            } else {
+                $nomor = $query->ids+1;
+                $seq = $nomor;
+            }
+        } else {
+            $seq = '0001';
+        }
+        return "PNG/".$thn.$bln."/".$seq;
+    }
+
+	public function edit_data($id) {
+		$query = $this->db->query("SELECT a.*, b.nama FROM pengeluaran a LEFT JOIN m_pengeluaran b ON b.id=a.idm_pengeluaran WHERE a.id = $id AND a.status_delete = 0")->row();
+        $response['value'] = [
+            'id' => $query->id,
+            'idm_pengeluaran' => $query->idm_pengeluaran,
+            'kode' => $query->kode,
+			'tanggal' => $query->tanggal,
+			'jumlah' => $query->jumlah,
+			'operator' => $query->operator,
+			'nama' => $query->nama,
+        ];
+
+        echo json_encode($response);
+	}
+
+	public function delete_data($id) {
+		$data['status_delete'] = 1;
+		$where = [
+			'id' => $id
+		];
+		$delete = $this->Modelku->update('pengeluaran', $data, $where);
+		if($delete<>false) {
+			$response['status'] = '200';
+			$response['title'] = 'Data telah berhasil dihapus';
+			$response['message'] = 'Data ini tidak akan dapat dipulihkan';
+		} else {
+			$response['status'] = '201';
+			$response['message'] = 'Data gagal dihapus';
+		}
+
+		echo json_encode($response);
+    }
+    public function get_select_jenis()
+    {
+        $this->db->select("a.id, CONCAT(a.nama) as text");
+        $this->db->where("a.status_delete", 0);
+        $this->db->like("CONCAT_WS(' ', a.nama)", $this->input->get('q'));
+        $this->db->limit(50);
+        $response["items"] = $this->db->get("m_pengeluaran a")->result();
+        
+        echo json_encode($response);
+    }
+
+}
---
+++ application/views/admin/pemasukan.php
@@ -0,0 +1,262 @@
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Master Pemasukan</h1>
+
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex">
+              <h6 class="m-0 font-weight-bold text-primary">Tabel Master Pemasukan</h6>
+              <a href="#" class="btn btn-primary btn-icon-split btn-sm ml-3" data-toggle="modal" data-target="#formModal">
+                <span class="icon text-white-50"><i class="fas fa-plus"></i></span>
+                <span class="text">Tambah data</span>
+              </a>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+              <table class="table table-bordered" id="table1" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th width="10%">No</th>
+                      <th width="10%">Kode</th>
+                      <th width="20%">Jenis</th>
+                      <th width="20%">tanggal</th>
+                      <th width="20%">jumlah</th>
+                      <th width="10%">Operator</th>
+                      <th width="10%">Action</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                  <tr>
+                    <th width="10%">No</th>
+                      <th width="10%">Kode</th>
+                      <th width="20%">Jenis</th>
+                      <th width="20%">tanggal</th>
+                      <th width="20%">jumlah</th>
+                      <th width="10%">Operator</th>
+                      <th width="10%">Action</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          
+          
+          </div>
+    <!-- /.container-fluid -->
+    
+    <!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form tambah data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="formAll">
+            <input type="hidden" name="id" id="id" value="">
+
+          <div class="form-group">
+            <label for="tanggal">Tanggal</label>
+            <input type="date" class="form-control" id="tanggal" name="tanggal" value="<?= date("Y-m-d") ?>">
+          </div>
+          <div class="form-group">
+            <label for="idm_pemasukan">Jenis</label><br>
+            <select name="idm_pemasukan" id="idm_pemasukan" class="form-control" style="width: 100%"></select>
+          </div>
+          <div class="form-group">
+            <label for="jumlah">Jumlah</label>
+            <input type="text" class="form-control" id="jumlah" name="jumlah">
+          </div>
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="save()">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        load_data();
+        load_select_jenis();
+        $('#formModal').on('hidden.bs.modal', function (e) {
+            reset_form();
+        });
+        /* Tanpa Rupiah */
+        var jumlah = document.getElementById('jumlah');
+        jumlah.addEventListener('keyup', function(e)
+        {
+            jumlah.value = formatrupiah(this.value);
+        });
+    });
+    /* Fungsi */
+    function formatrupiah(angka, prefix)
+    {
+        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+            split    = number_string.split(','),
+            sisa     = split[0].length % 3,
+            rupiah     = split[0].substr(0, sisa),
+            ribuan     = split[0].substr(sisa).match(/\d{3}/gi);
+            
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+        
+        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    }
+    
+    function load_data() {
+        $("#table1").DataTable({
+            destroy: true,
+            "processing" : true,
+            "serverSide" : true,
+            ajax :{
+                url: "<?= base_url('Pemasukan/load_data') ?>"
+            },
+            "columns" :[
+                {"name" : "id"},
+                {"name" : "kode"},
+                {"name" : "jenis"},
+                {"name" : "tanggal"},
+                {"name" : "jumlah"},
+                {"name" : "operator"},
+                {"name" : "action", "orderable": false, "searchlable": false, "className": "text-center"}
+            ],
+            "order" : [
+                [0, 'asc']
+            ],
+            "iDisplayLength" : 10
+        });
+    }
+
+    function save() {
+      var formData = new FormData($("#formAll")[0]);
+      $.ajax({
+        url: "<?= base_url('Pemasukan/tambah') ?>",
+        data: formData,
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        type: 'post'
+      }).done(function(data) {
+        if(data.status == 200) {
+          swal({
+            title: "Berhasil!",
+            text: data.message,
+            icon: "success",
+            button: "oke",
+          }).then((status)=>{
+            $("#formModal").modal('hide');
+            reset_form();
+            load_data();
+          });
+        } else if(data.status == 202) {
+          swal("Warning", data.message, "warning");
+        }
+      }) 
+    }
+
+    function reset_form() {
+      $("#id").val(0);
+      $("#idm_pemasukan option").remove();
+      $("#jumlah").val(0);
+      $("#tanggal").val("<?= date("Y-m-d") ?>");
+    }
+
+    function edit_data(id) {
+        $.ajax({
+            url: "<?= base_url('Pemasukan/edit_data/') ?>"+id,
+            dataType: 'json',
+            type: 'post'
+        }).done(function(data) {
+            $("#formModal").modal('show');
+            $("#tanggal").val(data.value.tanggal);
+            $("#jumlah").val(formatrupiah(data.value.jumlah));
+            if(Number(data.value.idm_pemasukan) > 0) {
+              $("#idm_pemasukan").append("<option value='"+data.value.idm_pemasukan+"'>"+data.value.nama+"</option>");
+            }
+            $("#id").val(data.value.id);
+        });
+    }
+
+    function delete_data(id){
+      swal({
+        title: "Anda yakin?",
+        text: "Data yang telah dihapus tidak dapat dipulihkan!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          $.ajax({
+            url: "<?= base_url('Pemasukan/delete_data/') ?>"+id,
+            dataType: 'json',
+            type: 'post'
+          }).done(function(data) {
+            if(data.status == 200) {
+              swal({
+                title: data.title,
+                text: data.message,
+                icon: 'success',
+                button: 'oke'
+              }).then((a)=>{
+                load_data();
+              });
+            } else {
+              swal('Error', data.message, 'error');
+            }
+          });
+        } else {
+          swal("Data Tidak jadi dihapus!");
+        }
+      });
+    }
+    function load_select_jenis(){
+        $('#idm_pemasukan').select2({
+            placeholder: 'Pilih Jenis Pemasukan',
+            multiple: false,
+            allowClear: true,
+            ajax: {
+                url: '<?php echo site_url("Pemasukan/get_select_jenis"); ?>',
+                dataType: 'json',
+                delay: 100,
+                cache: true,
+                data: function (params) {
+                    return {
+                        q: params.term, // search term
+                        page: params.page
+                    };
+                },
+                processResults: function (data, params) {
+                    params.page = params.page || 1;
+                    return {
+                        results: data.items,
+                        pagination: {
+                            more: (params.page * 30) < data.total_count
+                        }
+                    };
+                }
+            },
+            escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+            minimumInputLength: 0,
+            templateResult: FormatResult,
+            templateSelection: FormatSelection,
+        });
+    }
+</script>
